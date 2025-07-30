2fa075db6126abf2e476d5ed5f606c8636e8a06e
samuelmcdouall <samuelmcdouall@gmail.com>
2025-07-30 22:11:28 +0100

    Player moving with walk/idle animations

--- GMTK-Game-Jam-2025/Assets/Scripts/GJ25Player.cs
+++ GMTK-Game-Jam-2025/Assets/Scripts/GJ25Player.cs
@@ -8,21 +8,39 @@ public class GJ25Player : MonoBehaviour
     [SerializeField]
     InputActionReference _move;
     Vector3 _moveDirection;
+    [SerializeField]
+    Transform _modelTransform;
 
     [SerializeField]
     InputActionReference _interact;
+
+    [SerializeField]
+    Animator _ani;
+    int _idleAnimation;
+    int _moveAnimation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        _idleAnimation = Animator.StringToHash("Idle");
+        _moveAnimation = Animator.StringToHash("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
         _moveDirection = new Vector3(_move.action.ReadValue<Vector2>().x, 0.0f, _move.action.ReadValue<Vector2>().y);
-        //print(_moveDirection);
+
         transform.position += _moveSpeed * Time.deltaTime * _moveDirection;
+
+        if (_moveDirection != Vector3.zero)
+        {
+            _modelTransform.rotation = Quaternion.LookRotation(_moveDirection);
+            _ani.Play(_moveAnimation);
+        }
+        else
+        {
+            _ani.Play(_idleAnimation);
+        }
     }
 
     private void OnEnable()
